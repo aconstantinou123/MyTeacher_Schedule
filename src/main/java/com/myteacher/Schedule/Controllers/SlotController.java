61b052a937da738552fb5e90670388466ac42e5e
Alex Constantinou <alexconstantinou@Alexs-MBP.lan>
2018-12-01 00:36:46 +0000

    security added

--- src/main/java/com/myteacher/Schedule/Controllers/SlotController.java
+++ src/main/java/com/myteacher/Schedule/Controllers/SlotController.java
@@ -4,6 +4,7 @@ package com.myteacher.Schedule.Controllers;
 import com.myteacher.Schedule.Models.Slot;
 import com.myteacher.Schedule.Repositories.SlotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,9 +16,14 @@ public class SlotController {
     @Autowired
     SlotRepository slotRepository;
 
+//    @PreAuthorize("hasAuthority('TEACHER')")
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<Slot> getAllSlots() { return slotRepository.findAll(); }
+    public List<Slot> getAllSlots() {
+        System.out.println("here");
+        return slotRepository.findAll();
+    }
 
+    @PreAuthorize("hasAuthority('TEACHER')")
     @RequestMapping(value = "/{username}", method = RequestMethod.GET)
     public List<Slot> getByUserName(@PathVariable("username") String username){
         System.out.println("username " + username);
---
+++ src/main/java/com/myteacher/Schedule/Security/WebSecurity.java
@@ -0,0 +1,47 @@
+package com.myteacher.Schedule.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static com.myteacher.Schedule.Security.SecurityConstants.RELOAD_URL;
+
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, RELOAD_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()));
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With","Origin","Content-Type","Accept","Authorization"));
+
+        // This allow us to expose the headers
+        configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
